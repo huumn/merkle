f7fc5bfc4eb13ca231b376df57cf671677799378
keyan <keyan.kousha@gmail.com>
2018-12-28 17:02:34 -0800

    multiple hash functions is functional

--- merkle.h
+++ merkle.h
@@ -36,9 +36,30 @@ static inline uint32_t array_len(array_t *a) {
     2. wrappers around all openssl impls once we're linked
     3. make it clearer that input is actually double width hash_t
 */
-#define HASH_WIDTH 16
-typedef uint8_t merkle_hash_t[HASH_WIDTH];
-void hash_md5(merkle_hash_t input, merkle_hash_t output);
+typedef uint8_t *merkle_hash_t;
+typedef void (*cipher_func)(merkle_hash_t, merkle_hash_t);
+
+/* To add a new cipher, add an ACTION(<cipher name>, <width>) below.
+    This (1) declares a function
+        'void hash_<cipher name>(merkle_hash_t in, merkle_hash_t out)'
+        where 'in' is a double width merkle_hash_t (2*<width>) and out
+        is just <width>
+    (2) defines an enum in cipher_e
+        'CIPHER_<cipher name>'
+*/
+#define CIPHER_CODEC(ACTION) \
+    ACTION( MD5, 16 )        \
+
+#define CIPHER_ENUM(_name, _width) CIPHER_##_name,
+typedef enum cipher_e {
+    CIPHER_CODEC(CIPHER_ENUM)
+} cipher_e;
+#undef CIPHER_ENUM
+
+/* XXX Can we define this as cipher_func hash_##name? */
+#define CIPHER_FUNC(_name, _width) void hash_##_name(merkle_hash_t, merkle_hash_t);
+CIPHER_CODEC(CIPHER_FUNC)
+#undef CIPHER_FUNC
 
 /* Merkle Tree */
 typedef struct merkle_t {
@@ -48,10 +69,14 @@ typedef struct merkle_t {
     leaves are in levels[0] and if any node doesn't have a sibling
     they are the parent of themselves */
     array_t levels;
+
+    /* XXX abstract these out into a struct ?*/
+    cipher_func hash_func;
+    uint32_t hash_width;
 } merkle_t;
-merkle_err_t merkle_init(merkle_t *m);
+merkle_err_t merkle_init(merkle_t *m, cipher_e c);
 void merkle_deinit(merkle_t *m);
-merkle_hash_t *merkle_root(merkle_t *m);
+merkle_hash_t merkle_root(merkle_t *m);
 merkle_err_t merkle_add(merkle_t *m, merkle_hash_t hash);
 void merkle_print(merkle_t *m, int print_width);
 void merkle_print_hash(merkle_hash_t hash, int print_width);
@@ -64,10 +89,14 @@ typedef struct merkle_proof_t {
     with hashes[2] and so on ... such that the resulting hash
     is the root hash of its merkle tree ... leaf_idx tells
     us the starting concantenation order as we move to the root */
+    /* TODO: rename left_right */
     array_t pos;
     array_t hashes;
+
+    cipher_func hash_func;
+    uint32_t hash_width;
 } merkle_proof_t;
-merkle_err_t merkle_proof_init(merkle_proof_t *p);
+merkle_err_t merkle_proof_init(merkle_proof_t *p, cipher_e c);
 void merkle_proof_deinit(merkle_proof_t *p);
 merkle_err_t merkle_proof(merkle_proof_t *p, merkle_t *m, merkle_hash_t hash);
 merkle_err_t merkle_proof_validate(merkle_proof_t *p, merkle_hash_t root,
